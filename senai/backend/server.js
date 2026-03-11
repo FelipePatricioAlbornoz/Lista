9dcb6af2755cb067864a336b76480d2d5df3d6e2
FelipePatricioAlbornoz <felipe_albornoz@estudante.sesisenai.org.br>
2026-03-10 23:16:09 -0300

    sql

--- senai/backend/server.js
+++ senai/backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 const useRoutes = require("./routes/users");
 const taskRoutes = require("./routes/tasks");
 
@@ -8,8 +9,11 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/users", useRoutes);
 app.use("/tasks", taskRoutes);
+app.use("/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
